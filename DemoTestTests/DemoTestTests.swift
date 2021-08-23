af5c3397284e8f122ea9c6496e3af0701fad24ff
reetika sharma <reetika@secninjaz.com>
2021-08-23 20:20:45 +0530

    Initial Commit

---
+++ DemoTestTests/DemoTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  DemoTestTests.swift
+//  DemoTestTests
+//
+//  Created by Reetika Sharma on 23/08/21.
+//
+
+import XCTest
+@testable import DemoTest
+
+class DemoTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
